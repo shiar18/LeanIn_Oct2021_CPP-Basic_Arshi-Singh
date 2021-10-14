21234b5aa9ff3409fe651cd7d31cc7e968f9febd
shiar18 <91905554+shiar18@users.noreply.github.com>
2021-10-14 13:25:09 +0530

    Add files via upload

---
+++ Milestone 1/Post_Pre_Operations.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+ int main() 
+ {
+     int a;
+     cout<<"Enter a Number ";
+     cin>>a;
+     cout<<"Number entered by the user"<<endl<<a<<endl;
+     cout<<a++<<endl;
+     cout<<++a<<endl;
+     cout<<a--<<endl;
+     cout<<--a<<endl;
+     return 0;
+ }
+
+ // 
+Enter a Number 2
+Number entered by the user
+2
+2
+4
+4
+2
