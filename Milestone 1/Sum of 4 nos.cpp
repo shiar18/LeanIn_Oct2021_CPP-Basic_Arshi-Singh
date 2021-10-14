b2585f115dd8bb1a07b75de4fd4222ff0f5e8f85
shiar18 <91905554+shiar18@users.noreply.github.com>
2021-10-14 13:26:20 +0530

    Add files via upload

---
+++ Milestone 1/Sum of 4 nos.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a , b , c , d , sumofFourNumbers;
+     cout<<"Enter four integers: ";
+     cin>> a >> b >> c >> d;
+     // sum of four numbers is stored in variable sumofFourNumbers;
+     sumofFourNumbers = a + b + c + d;
+     // Prints sum
+     cout<<a<<" + "<<b<<" + "<<c<<" + "<<d<<" = "<<sumofFourNumbers;
+               return 0;
+}
+
+//Enter four integers: 1 2 3 4
+1 + 2 + 3 + 4 = 10
