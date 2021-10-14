aae8738497920518c7d4ea362eec174d99aa76dd
shiar18 <91905554+shiar18@users.noreply.github.com>
2021-10-14 13:27:11 +0530

    Add files via upload

---
+++ Milestone 1/Size Of Datatype.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+    cout<<"Size of char: " <<sizeof(char)<< " byte" << endl;
+    cout<<"Size of int: " <<sizeof(int) << "bytes" << endl;
+    cout<<"Size of float" <<sizeof(float) << "bytes" << endl;
+    cout<<"Size of double" <<sizeof(double)<< "bytes" <<endl;
+return 0;
+}
+
+//Size of char: 1 byte
+Size of int: 4bytes 
+Size of float4bytes 
+Size of double8bytes
