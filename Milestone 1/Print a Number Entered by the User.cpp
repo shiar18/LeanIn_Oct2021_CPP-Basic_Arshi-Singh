5a6e6274aadadcb323cea9b7ee9ae21d91c25a4c
shiar18 <91905554+shiar18@users.noreply.github.com>
2021-10-14 23:19:43 +0530

    Add files via upload

---
+++ Milestone 1/Print a Number Entered by the User.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {    
+    int number;
+
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    cout << "You entered " << number;    
+    return 0;
+}
+
+//Enter an integer: 5
+You entered 5
