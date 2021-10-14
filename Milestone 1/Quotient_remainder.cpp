c711976287970b65187c89ab067814ba78585e3e
shiar18 <91905554+shiar18@users.noreply.github.com>
2021-10-14 13:26:48 +0530

    Add files via upload

---
+++ Milestone 1/Quotient_remainder.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b, quotient, remainder;
+    cout << "Enter a\n";
+    cin >> a;
+    cout << "Enter b\n";
+    cin >> b;
+    quotient = a/b;
+    remainder= a%b;
+    cout<<"Quotient is" << quotient << endl;
+cout<<"Remainder is" << remainder;
+return 0;
+}                                                         
+
+//Enter Numerator
+9
+Enter Denominator
+2
+Quotient is4 
+Remainder is1
