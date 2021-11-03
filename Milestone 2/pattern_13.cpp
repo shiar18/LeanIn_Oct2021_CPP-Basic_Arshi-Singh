8315c7c4270b51541a78168a3630c7d249affde3
shiar18 <91905554+shiar18@users.noreply.github.com>
2021-11-03 17:05:06 +0530

    Add files via upload

---
+++ Milestone 2/pattern_13.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter value of n";
+    cin>>n;
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=2*n-1;j++)
+        {if(j>=n-(i-1) && j<=n+(i-1))
+        cout<<"*";
+        else
+        cout<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
+//
+Enter value of n5
+
+     *
+    ***
+   *****
+  *******
+ *********
+
