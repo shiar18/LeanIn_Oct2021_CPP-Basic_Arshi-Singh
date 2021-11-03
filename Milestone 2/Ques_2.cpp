81b3d789000b37bf73541e565f9485a527ffca20
shiar18 <91905554+shiar18@users.noreply.github.com>
2021-11-03 17:05:42 +0530

    Add files via upload

---
+++ Milestone 2/Ques_2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=50;i++){
+        if(i%3==0)
+        cout<<i<<endl;
+
+    }
+    return 0;
+
+}
+//
+3
+6
+9
+12
+15
+18
+21
+24
+27
+30
+33
+36
+39
+42
+45
+48
