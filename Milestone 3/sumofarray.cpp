0ad0891e40152ed507ccb2b194c770f450530686
shiar18 <91905554+shiar18@users.noreply.github.com>
2021-11-15 14:24:06 +0530

    Add files via upload

---
+++ Milestone 3/sumofarray.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n,sum=0;
+    int arr[]={1,3,5,7,9};
+    n=5;
+    for(i=0;i<n;i++){
+        sum+=arr[i];
+        }
+        cout<<"Sum of the numbers in the array ";
+        cout<<sum;
+        return 0;
+}
+
+//
+Sum of the numbers in the array 25
