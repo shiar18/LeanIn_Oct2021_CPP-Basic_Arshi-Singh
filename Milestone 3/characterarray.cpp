9a7a5f1d7ea2ee5eb898052c1eccfa5bceef0778
shiar18 <91905554+shiar18@users.noreply.github.com>
2021-11-15 14:50:48 +0530

    Add files via upload

---
+++ Milestone 3/characterarray.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char name[100];
+    cout<<"Enter a string: ";
+    cin>>name;
+    cout<<"You entered: "<<name<<endl;
+    return 0;
+}
+//
+Enter a string: hello
+You entered: hello
